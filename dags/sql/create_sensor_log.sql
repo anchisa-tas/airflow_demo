c6325b85bcfde6cd341561c0f1629152f9cab5fa
Anchisa Tassanaviriyakul <60860@KPC-An-An-Macbook-Pro.local>
2025-08-26 13:13:52 +0700

    Update sql

--- dags/sql/create_sensor_log.sql
+++ dags/sql/create_sensor_log.sql
@@ -1,13 +1,7 @@
 CREATE TABLE IF NOT EXISTS sensor_log (
     department_name VARCHAR(32) NOT NULL,
     sensor_serial VARCHAR(64) NOT NULL,
-    create_at DATE NOT NULL,
+    create_at TIMESTAMP NOT NULL,
     product_name VARCHAR(16) NOT NULL,
-    product_expire DATE NOT NULL
+    product_expire TIMESTAMP NOT NULL
 );
-
--- CREATE INDEX IF NOT EXISTS idx_sensor_log_department_name ON sensor_log(department_name);
--- CREATE INDEX IF NOT EXISTS idx_sensor_log_sensor_serial ON sensor_log(sensor_serial);
--- CREATE INDEX IF NOT EXISTS idx_sensor_log_create_at ON sensor_log(create_at);
--- CREATE INDEX IF NOT EXISTS idx_sensor_log_product_name ON sensor_log(product_name);
--- CREATE INDEX IF NOT EXISTS idx_sensor_log_product_expire ON sensor_log(product_expire);
